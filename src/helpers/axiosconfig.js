bcc0abe786554f351fe3650ef989562d5c00af90
unknown <dev23.mxpertz@gmail.com>
2024-05-04 11:47:37 +0530

    api changes

--- src/helpers/axiosconfig.js
+++ src/helpers/axiosconfig.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  // baseURL: "http://localhost:3000/api/",
   // baseURL: "/Backend_Speakable_English/api/",
-  // baseURL:"https://new-backend-speakable.onrender.com/api/",
+  baseURL: "https://new-backend-speakable.onrender.com/api/",
   withCredentials: true,
 });
 
